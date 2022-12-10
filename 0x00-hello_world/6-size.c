666113f6e52ac001e9547c81c2db92eab24c18be
olaniyi <olaniyioladipo@hotmail.com>
2022-12-10 05:58:40 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,4 +15,5 @@ int main(void)
 	printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int)):
 	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	return (0);
 }
