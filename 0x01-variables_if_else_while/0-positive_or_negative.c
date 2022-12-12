eb997da235935681182e2e2198aa0df36c945c70
olaniyi <olaniyioladipo@hotmail.com>
2022-12-12 08:03:48 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 	
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	 printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -30,4 +30,3 @@ int main(void)
 	}
 	return (0);
 }
-
