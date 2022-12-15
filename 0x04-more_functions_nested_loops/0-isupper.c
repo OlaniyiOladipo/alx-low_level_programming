7b71c1a9801c0f9c6485715067e9e4cfe1479e0e
olaniyi <olaniyioladipo@hotmail.com>
2022-12-15 15:56:11 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- *_isuppler - checks if parameter is an upper class
- * @c: print int
- * Return: 1 if is an uppercase character, 0 in other case
+ * _isupper - checks if parameter is an uppercase character
+ * @c: input character
+ * Return: 1 if there is an uppercase character, 0 in other case
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
-else
-{
-return (0);
-}
-
-}
-
