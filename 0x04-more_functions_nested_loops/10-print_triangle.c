dfd8b80b444ba140f8aece30c086caa02d8df4e4
olaniyi <olaniyioladipo@hotmail.com>
2022-12-15 16:48:58 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: numbers of line
+ * Return: no return
+ */
+
+void print_triangle(int size)
+
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < (size - 1); j++)
+			_putchar(' ');
+		for (j--; j < side; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
