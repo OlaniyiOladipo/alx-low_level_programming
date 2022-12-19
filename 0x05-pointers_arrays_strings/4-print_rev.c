f1524ba9d5240527bb63178077a3ae0d76bb32a9
olaniyi <olaniyioladipo@hotmail.com>
2022-12-19 06:40:32 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,18 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse order
- * @str: a pointer to an int that will be chnaged
- *
- * Return: void which means our answer is correct
+ * print_rev - prints a string in reverse
+ * @str: string to be printed
  */
 
 void print_rev(char *s)
 {
-int i;
-
-i = 0;
-while (s[i] != '\0')
-{
-1++;
-}
-
-for (i = i - 1 : i >= 0; i--)
-{
-_putchar (s[i]);
-}
-
-_putchar ('\n');
+	int i = 0;
+	
+	while (s[i] != '\0')
+		1++;
+	for (i = i - 1 : i >= 0; i--)
+		_putchar (s[i]);
+	
+	_putchar ('\n');
 }
