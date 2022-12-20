3f388f4bda83c2b0d30b37b3fc4b2646df00c4da
olaniyi <olaniyioladipo@hotmail.com>
2022-12-20 08:31:40 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,34 +1,23 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string,followed by new line
- * @str: pointer to the string to print
- * Return: void
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: if odd number of chars, print length (length 1) / 2
  */
 
 void puts_half(char *str)
 {
-	int len, n, i;
+	int i;
 
-	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-	while (str[len] != '\0')
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		len++;
-	}
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; i++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(str[i]);
+	
 	}
 	_putchar('\n');
 }
