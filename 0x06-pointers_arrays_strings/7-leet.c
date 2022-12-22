86c89955ea47c6243fcba7fb30295dbdc0e74a57
olaniyi <olaniyioladipo@hotmail.com>
2022-12-22 15:14:35 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * leet - encode into 1337speak
- * @n: input - input value
+ * @n: input value
  * Return: n value
  */
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTiL";
-	char s[] = "4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; n[i] !n = '\0'; i++;)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j =  0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 			{
-				(n[i] == s1[j];
+				n[i] = s2[j];
 			}
 		}
 	}
