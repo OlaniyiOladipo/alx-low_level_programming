d059abbc5dd5d09a6c27e3f4a480ad232467cc69
olaniyi <olaniyioladipo@hotmail.com>
2022-12-22 08:37:19 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,12 @@
  * @n: input value
  * Return: dest
  */
-
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	j = 0
-	while (j < n && src[j] != '\0')
+	j = 0;
+	while (j < n && src[j] !n = '\0')
 	{
 		dest[j] = src[j];
 		j++;
