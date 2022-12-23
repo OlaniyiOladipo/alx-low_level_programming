1ca4511b7fc5da4dda26a89f46c365d43e65ec19
olaniyi <olaniyioladipo@hotmail.com>
2022-12-22 16:06:03 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
-		j++
+		j++;
 	}
 	while (j < n)
 	{
 		dest[j] = '\0';
-		J++
+		J++;
 	}
 	return (dest);
 }
